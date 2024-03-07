6b48e153f14d358e93fd6b735b70625539ff12b0
Arsen Trynko <arsen@shaper.us>
2024-03-07 12:55:49 +0200

    changed enums value to strings

--- src/constants/auth/enums.ts
+++ src/constants/auth/enums.ts
@@ -1,6 +1,6 @@
 export enum UserRole {
-  Customer,
-  Admin,
+  User = 'user',
+  Admin = 'admin',
 }
 
 export enum AuthAccessLevel {
@@ -10,6 +10,7 @@ export enum AuthAccessLevel {
 }
 
 export enum UserStatus {
-  Active,
-  Blocked,
+  Pending = 'pending',
+  Active = 'active',
+  Blocked = 'blocked',
 }
