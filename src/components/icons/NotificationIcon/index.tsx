f97224571df34974b155fd6e6de6cdf7de10fdef
viktoria-monastyrska <viktoria@shaper.us>
2024-03-25 18:02:57 +0200

    fix: code restructure

--- src/components/icons/NotificationIcon/index.tsx
+++ src/components/icons/NotificationIcon/index.tsx
@@ -11,7 +11,7 @@ const NotificationIcon = ({
   viewBox = '0, 0, 32, 32',
   width = '24',
   height = '24',
-  active = false,
+  disabled,
   direction = 'up',
   sx,
   ...rest
@@ -26,25 +26,25 @@ const NotificationIcon = ({
     >
       <path
         d="M20 25C20 27.2091 18.2091 29 16 29C13.7909 29 12 27.2091 12 25H20Z"
-        fill={active ? '#242229' : theme.palette.grey[200]}
+        fill={disabled ? theme.palette.grey[200] : '#242229'}
       />
-      {active ? (
+      {disabled ? (
         <path
-          fill-rule="evenodd"
+          xmlns="http://www.w3.org/2000/svg"
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M16 4C11.5817 4 8 7.58172 8 12V18C8 18.5523 7.55228 19 7 19C6.44772 19 6 19.4477 6 20V22C6 22.5523 6.44772 23 7 23H8H24H25C25.5523 23 26 22.5523 26 22V20C26 19.4477 25.5523 19 25 19C24.4477 19 24 18.5523 24 18V12C24 7.58172 20.4183 4 16 4Z"
+          fill={theme.palette.grey[200]}
+        />
+      ) : (
+        <path
+          fillRule="evenodd"
           clip-rule="evenodd"
           d="M16.1889 4.41425C16.3138 4.24698 16.2087 4 16 4C11.5817 4 8 7.58172 8 12V18C8 18.5523 7.55228 19 7 19C6.44772 19 6 19.4477 6 20V22C6 22.5523 6.44772 23 7 23H8H24H25C25.5523 23 26 22.5523 26 22V20C26 19.4477 25.5523 19 25 19C24.4477 19 24 18.5523 24 18V14.931C24 14.1609 23.1444 13.6585 22.3951 13.837C21.9475 13.9436 21.4803 14 21 14C17.6863 14 15 11.3137 15 8C15 6.65567 15.4421 5.41459 16.1889 4.41425Z"
           fill="#242229"
         />
-      ) : (
-        <path
-          xmlns="http://www.w3.org/2000/svg"
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-          d="M16 4C11.5817 4 8 7.58172 8 12V18C8 18.5523 7.55228 19 7 19C6.44772 19 6 19.4477 6 20V22C6 22.5523 6.44772 23 7 23H8H24H25C25.5523 23 26 22.5523 26 22V20C26 19.4477 25.5523 19 25 19C24.4477 19 24 18.5523 24 18V12C24 7.58172 20.4183 4 16 4Z"
-          fill={theme.palette.grey[200]}
-        />
       )}
-      {active ? <circle cx="21" cy="8" r="4" fill="#886FFF" /> : null}
+      {!disabled ? <circle cx="21" cy="8" r="4" fill="#886FFF" /> : null}
     </SvgIcon>
   );
 };
--- src/components/icons/types.ts
+++ src/components/icons/types.ts
@@ -3,5 +3,4 @@ import type { SvgIconProps } from '@mui/material/SvgIcon';
 export interface ISvgProps extends SvgIconProps {
   direction?: 'up' | 'down' | 'left' | 'right';
   disabled?: boolean;
-  active?: boolean;
 }
