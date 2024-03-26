2fe7eb0eafce5547b48a5ed2b02a5d9ebde7f02d
viktoria-monastyrska <viktoria@shaper.us>
2024-03-26 10:45:10 +0200

    fix: code structure

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,6 +1,5 @@
 import { AppBar, Divider, Toolbar } from '@mui/material';
 import { useRouter } from 'next/router';
-import { useMemo } from 'react';
 
 import { ArcadeIcon, DashboardIcon, NotificationIcon } from '@/components/icons';
 
@@ -11,44 +10,29 @@ const Header = () => {
   const router = useRouter();
 
   const handleClick = (redirectUrl: string) => () => router.push(redirectUrl);
-  const headerTabs = useMemo(
-    () => [
-      {
-        children: <NotificationIcon width={32} height={32} disabled />,
-        disabled: true,
-        redirectUrl: '/notification',
-        isCurrentPath: router.pathname === '/notification',
-      },
-      {
-        children: <DashboardIcon width={32} height={32} />,
-        disabled: false,
-        redirectUrl: '/',
-        isCurrentPath: router.pathname === '/',
-      },
-      {
-        children: <ArcadeIcon width={32} height={32} disabled />,
-        disabled: true,
-        redirectUrl: '/arcade',
-        isCurrentPath: router.pathname === '/arcade',
-      },
-    ],
-    [router.pathname],
-  );
+
   return (
     <AppBar sx={styles.appBar}>
       <Toolbar sx={styles.toolbar}>
         <AuthPopup />
-        {headerTabs.map((tab, index) => (
-          <CategoryButton
-            key={index}
-            disabled={tab.disabled}
-            onClick={handleClick(tab.redirectUrl)}
-            selected={tab.isCurrentPath}
-          >
-            {tab.children}
-          </CategoryButton>
-        ))}
+        <CategoryButton
+          disabled
+          onClick={handleClick('/notification')}
+          selected={router.pathname === '/notification'}
+        >
+          <NotificationIcon width={32} height={32} disabled />
+        </CategoryButton>
         <Divider sx={styles.divider} />
+        <CategoryButton onClick={handleClick('/')} selected={router.pathname === '/'}>
+          <DashboardIcon width={32} height={32} />
+        </CategoryButton>
+        <CategoryButton
+          disabled
+          onClick={handleClick('/arcade')}
+          selected={router.pathname === '/arcade'}
+        >
+          <ArcadeIcon width={32} height={32} disabled />
+        </CategoryButton>
       </Toolbar>
     </AppBar>
   );
