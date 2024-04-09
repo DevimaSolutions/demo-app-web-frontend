94c45ee2b2fec4ca242b808bd4211533fccee29b
viktoria-monastyrska <viktoria@shaper.us>
2024-04-09 16:01:02 +0300

    fix: switch skill progress component

--- src/components/RoundTabsGroup/styles.ts
+++ src/components/RoundTabsGroup/styles.ts
@@ -1,22 +1,31 @@
+import { tabClasses, tabsClasses } from '@mui/material';
+
 import { theme } from '@/constants';
 
 const styles = {
   root: {
-    backgroundColor: '#F4F4F4',
+    backgroundColor: theme.palette.grey[400],
     borderRadius: 2,
     p: 0.5,
     minHeight: 'unset',
-    ['.Mui-selected']: {
-      color: theme.palette.primaryDark[100],
+
+    [`& .${tabsClasses.flexContainer}`]: {
+      border: 'none',
     },
   },
   tab: {
-    p: theme.spacing(0.95, 3.375),
-    minHeight: 'unset',
-    zIndex: 1,
-    color: theme.palette.grey[600],
-    fontSize: 14,
-    fontWeight: 500,
+    [`&.${tabClasses.root}`]: {
+      p: theme.spacing(0.75, 3.375),
+      minHeight: 'unset',
+      zIndex: 1,
+      fontSize: 14,
+      fontWeight: 500,
+      lineHeight: '20px',
+      color: theme.palette.grey[600],
+    },
+    [`&.${tabClasses.root}.Mui-selected`]: {
+      color: theme.palette.primaryDark[100],
+    },
   },
   indicator: {
     backgroundColor: theme.palette.common.white,
